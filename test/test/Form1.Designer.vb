28ecc708c3f30ba53c20bba2a2fc09ecac0fc5e4
thunt1 <thunt1@una.edu>
2014-01-27 21:30:37 -0600

    Travis test collision

--- test/test/Form1.Designer.vb
+++ test/test/Form1.Designer.vb
@@ -41,7 +41,7 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(12, 19)
+        Me.Label1.Location = New System.Drawing.Point(62, 9)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(39, 13)
         Me.Label1.TabIndex = 1
@@ -49,7 +49,7 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(118, 43)
+        Me.Button1.Location = New System.Drawing.Point(26, 25)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 2
@@ -67,7 +67,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(202, 76)
+        Me.ClientSize = New System.Drawing.Size(366, 155)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label1)
